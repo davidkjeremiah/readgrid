8dbf99257be9ef5ac0a3df323d47c8d6ecce3204
davidkjeremiah <davidkjerry@gmail.com>
2025-10-06 22:22:14 +0100

    upgrade version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="readgrid",
-    version="0.1.9",
+    version="0.2.0",
     author="David Jeremiah",
     author_email="flasconnect@gmail.com",
     description="A document layout pipeline for detecting tables, images, and structured extraction.",
